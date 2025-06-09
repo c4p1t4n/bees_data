80df2b48ed83a06d2ba5ea8555b318e4dca5f6a5
leo oliveira <leoigornunes@protonmail.com>
2025-06-08 22:19:18 -0300

    add logging to extract_data_api for better error handling and process tracking

--- docker/src/extract_data_api.py
+++ docker/src/extract_data_api.py
@@ -1,9 +1,11 @@
+# src/extract_data_api.py
 import requests
 import json
 import os
 from datetime import datetime
 import boto3
 from botocore.client import Config
+from airflow.utils.log.logging_mixin import LoggingMixin
 
 def extract_breweries_to_minio(bucket_name: str, object_key_prefix: str, execution_date: str = None) -> str:
     """
@@ -17,8 +19,12 @@ def extract_breweries_to_minio(bucket_name: str, object_key_prefix: str, executi
     Returns:
         str: Caminho (key) do arquivo salvo no S3.
     """
+    logger = LoggingMixin().log
+
     if not execution_date:
         execution_date = datetime.today().strftime("%Y-%m-%d")
+
+    logger.info(f"Iniciando extração da API Open Brewery DB - Data: {execution_date}")
 
     all_breweries = []
     page = 1
@@ -27,36 +33,25 @@ def extract_breweries_to_minio(bucket_name: str, object_key_prefix: str, executi
     response = requests.get(url)
 
     if response.status_code != 200:
+        logger.error(f"Erro ao acessar API: {response.status_code} - {response.text}")
         raise Exception(f"Erro ao acessar API: {response.status_code} - {response.text}")
 
     breweries = response.json()
+    logger.info(f"{len(breweries)} registros obtidos da API")
 
-
-    # Serializa JSON como string
     json_data = json.dumps(breweries, ensure_ascii=False, indent=2)
-
-    # Define a key (caminho no bucket)
     object_key = f"{object_key_prefix}/breweries_{execution_date}.json"
 
-    # Conecta ao MinIO/S3
     s3 = boto3.client(
         "s3",
         endpoint_url=os.getenv("S3_ENDPOINT_URL", "http://localhost:9000"),
-        aws_access_key_id="H1Cv8fzwXljv2Vl3RO79",
-        aws_secret_access_key="nQCkdSU2GHSnjC36shOijpSZuIZxGc8BNsHRBU0A",
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID", "H1Cv8fzwXljv2Vl3RO79"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY", "nQCkdSU2GHSnjC36shOijpSZuIZxGc8BNsHRBU0A"),
         config=Config(signature_version="s3v4"),
         region_name="us-east-1"
     )
 
-    # Faz upload do JSON diretamente
     s3.put_object(Bucket=bucket_name, Key=object_key, Body=json_data.encode("utf-8"))
 
-    print(f"[INFO] Upload finalizado com sucesso: s3://{bucket_name}/{object_key}")
+    logger.info(f"Upload finalizado com sucesso: s3://{bucket_name}/{object_key}")
     return object_key
-
-
-if __name__ == "__main__":
-    extract_breweries_to_minio(
-        bucket_name="datalake",
-        object_key_prefix="bronze/breweries"
-    )
